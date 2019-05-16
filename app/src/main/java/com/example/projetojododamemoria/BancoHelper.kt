c73dd96cfe4fcb3681bfc2e45882c17f3f787275
Kamila <kmilaporto1991@gmail.com>
2019-05-15 23:44:20 -0300

    ajusta banco

--- app/src/main/java/com/example/projetojododamemoria/BancoHelper.kt
+++ app/src/main/java/com/example/projetojododamemoria/BancoHelper.kt
@@ -5,10 +5,10 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class BancoHelper (context: Context):
-    SQLiteOpenHelper(context, "banco", null, 1){
+    SQLiteOpenHelper(context, "teste", null, 1){
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val sql = "create table pessoa(id integer primary key autoincrement, nome text, disciplinas text, img text)"
+        val sql = "create table professor(id integer primary key autoincrement, nome text, disciplinas text, img text)"
         db?.execSQL(sql)
     }
 
